93e81d3f32160be0d762d027e06ef0b369e5f40d
Martin Weber Nissle <webernissle@gmail.com>
2025-04-20 13:11:50 +0200

    Update dev-docker-init.ps1

--- gpt_backend/dev-docker-init.ps1
+++ gpt_backend/dev-docker-init.ps1
@@ -8,12 +8,5 @@ Copy-Item -Path "template-dotenv-secrets.txt" -Destination ".env" -Force
 # Append the contents of template-dotenv.txt to .env
 Get-Content -Path "template-dotenv.txt" | Add-Content -Path ".env"
 
-# Replace the values in .env with the values from the secrets file
-Get-Content -Path "template-dotenv-secrets.txt" | ForEach-Object {
-    $line = $_
-    $key, $value = $line -split '=', 2
-    (Get-Content -Path ".env") -replace "^\s*$key=.*", "$key=$value" | Set-Content -Path ".env"
-}
-
 # Start Docker Compose
 docker-compose up -d
