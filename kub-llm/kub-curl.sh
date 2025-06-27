ce519d72998041f94e62981fb771243b0421b2a9
Nelo Nissle <nelonissle@gmail.com>
2025-06-27 09:25:29 +0200

    wip

--- kub-llm/kub-curl.sh
+++ kub-llm/kub-curl.sh
@@ -1,21 +1,36 @@
 #!/usr/bin/env bash
 
+export LLMHOST=192.168.9.181
+
 echo "kubectl forward port to localhsot:"
 # microk8s kubectl port-forward svc/llm-ollama-tinyllama 11434:11434 -n llm &
 
+microk8s kubectl get svc -n llm
+# read ip adress of the service on command line to variable LLMHOST
 echo
-echo "ollama check status:"
-curl http://localhost:11434
+echo "Enter the IP address of the LLM service (e.g., 192.168.9.181):"
+read LLMHOST
+
+echo
+echo "ollama check status on host $LLMHOST:"
+curl http://$LLMHOST:11434
 
 echo
 echo "ollama install model:"
-curl -X POST http://localhost:11434/api/pull \
+curl -X POST http://$LLMHOST:11434/api/pull \
      -H "Content-Type: application/json" \
      -d '{"name":"llama3"}'
-     
 
-curl http://localhost:11434/api/tags
-
-curl -X POST http://localhost:11434/api/generate \
+echo
+echo "ollama check status after installing model:"     
+curl -X POST http://$LLMHOST:11434/api/generate \
   -H "Content-Type: application/json" \
   -d '{"model": "llama3", "prompt": "Hello, world!"}'
+
+echo
+echo "ollama list models:"
+# read tags from curl and convert output to json and print models[0].name and supress other output
+curl -sS http://$LLMHOST:11434/api/tags | jq '.models[] | .name'
+
+echo
+echo "done."
